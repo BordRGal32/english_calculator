1f43f43988d8b093c4d9912d8286d1cd9ef945b1
Robin Miller & Lauren Yeiser <student@epicodus.com>
2014-03-06 11:42:15 -0800

    fully functional code does not do order of operations

--- lib/english_calculator.rb
+++ lib/english_calculator.rb
@@ -1,18 +1,32 @@
 def english_calculator(number_string)
-  operators = {"plus" => :+, "minus" => :- }
-  words_array = number_string.split(' ')
-  numbers = []
-  found_operators = []
 
-  words_array.each do |word|
-    if (/\d/ =~ word) != nil
-      numbers.push(/\d+/.match(word)[0])
-    elsif operators[word] != nil
-      found_operators.push(operators[word])
+  questions = number_string.split("?")
+  final_results = []
+  questions.each do |question|
+
+    operators = {"plus" => :+, "minus" => :-, "times" => :*, "divided" => :/, "modulo" => :%, "power" => :**}
+    words_array = question.split(' ')
+    numbers = []
+    found_operators = []
+
+    words_array.each do |word|
+      if !(/[\d\.]/ =~ word).nil?
+        numbers.push(/[\d\.]+/.match(word)[0])
+      elsif !operators[word].nil?
+        found_operators.push(operators[word])
+      end
     end
+    current_result = numbers[0].to_f
+
+    1.upto(numbers.length-1) do |index|
+      current_operator = found_operators[index - 1]
+      current_result = [current_result, numbers[index].to_f].inject(current_operator)
+    end
+    current_result = current_result.round(1)
+    final_results.push(current_result)
   end
-  plus_sign = found_operators[0]
-  [numbers[0].to_i, numbers[1].to_i].inject(plus_sign)
+    final_results
 end
+
+puts RubyVM::InstructionSequence.compile("2+4" ).eval 
  
-# puts english_calculator("what is 2 plus 5")
--- spec/english_calculator_spec.rb
+++ spec/english_calculator_spec.rb
@@ -3,12 +3,36 @@ require('english_calculator')
 
 describe("english_calculator") do
   it("will add two numbers entered as a string") do
-    english_calculator("what is 2 plus 5").should(eq(7))
+    english_calculator("what is 2 plus 5").should(eq([7]))
   end 
   it("will add two double digit numbers entered as a string") do
-    english_calculator("what is 25 plus 25").should(eq(50))
+    english_calculator("what is 25 plus 25").should(eq([50]))
   end 
   it("will subtract a digit number entered as a string") do
-    english_calculator("what is 25 minus 24").should(eq(1)) 
+    english_calculator("what is 25 minus 24").should(eq([1])) 
+  end
+  it('will multiply two single digit numbers entered as a string') do
+    english_calculator("what is 4 times 9").should(eq([36]))
+  end
+  it('will multiply two double digit numbers entered as a string') do
+    english_calculator("what is 15 times 10").should(eq([150]))
+  end
+  it('will divide two single digit numbers') do
+    english_calculator('what is 20 divided by 4').should(eq([5]))
+  end
+  it('will divide two float numbers') do
+    english_calculator('what is 20.4 divided by 4.1').should(eq([5]))
+  end
+  it("will find the modulo of two numbers") do
+    english_calculator("what is 7 modulo 2").should(eq([1]))
+  end
+  it("will find powers of a number") do
+    english_calculator("what is 2 to the 5th power").should(eq([32]))
+  end
+  it("will find the result of multiple operations") do
+    english_calculator("what is 3 times 4 plus 2").should(eq([14]))
+  end
+  it("will answer multiple questions") do
+    english_calculator("what is 3 times 4? what is 3 plus 4").should(eq([12,7]))
   end
 end
