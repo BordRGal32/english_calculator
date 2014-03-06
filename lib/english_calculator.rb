d5aacc3923b71f6917fbb2b23f43b096a94e9677
Robin Miller & Lauren Yeiser <student@epicodus.com>
2014-03-06 09:59:09 -0800

    can add and subtract two numbers. Three specs written

--- lib/english_calculator.rb
+++ lib/english_calculator.rb
@@ -1,6 +1,18 @@
 def english_calculator(number_string)
-  operands = {"plus" => :+}
-  puts operands["plus"]
+  operators = {"plus" => :+, "minus" => :- }
+  words_array = number_string.split(' ')
+  numbers = []
+  found_operators = []
+
+  words_array.each do |word|
+    if (/\d/ =~ word) != nil
+      numbers.push(/\d+/.match(word)[0])
+    elsif operators[word] != nil
+      found_operators.push(operators[word])
+    end
+  end
+  plus_sign = found_operators[0]
+  [numbers[0].to_i, numbers[1].to_i].inject(plus_sign)
 end
  
-english_calculator("plus")
+# puts english_calculator("what is 2 plus 5")
--- spec/english_calculator_spec.rb
+++ spec/english_calculator_spec.rb
@@ -5,4 +5,10 @@ describe("english_calculator") do
   it("will add two numbers entered as a string") do
     english_calculator("what is 2 plus 5").should(eq(7))
   end 
+  it("will add two double digit numbers entered as a string") do
+    english_calculator("what is 25 plus 25").should(eq(50))
+  end 
+  it("will subtract a digit number entered as a string") do
+    english_calculator("what is 25 minus 24").should(eq(1)) 
+  end
 end
